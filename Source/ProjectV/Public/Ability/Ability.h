00fd4e4c34d7bb1adc071033cf0ed42ad48200d0
purohatsu <dnwls010728@gmail.com>
2025-06-08 22:58:38 +0900

    FEAT: 수치 조정 및 어빌리티 추가

---
+++ Source/ProjectV/Public/Ability/Ability.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Ability.generated.h"
+
+UCLASS()
+class PROJECTV_API AAbility : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAbility();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnAttack();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
